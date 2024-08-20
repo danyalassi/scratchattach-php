0cb770c6aefd2eab8045de74162f3b0f5da64320
danyalassi <70813522+danyalassi@users.noreply.github.com>
2024-08-21 00:03:43 +0200

    Update __init__.py
    Signed-off-by: danyalassi <70813522+danyalassi@users.noreply.github.com>

--- scratchattach/__init__.py
+++ scratchattach/__init__.py
@@ -8,25 +8,25 @@ from .forum import *
 from .encoder import *
 
 def get_news(*, limit=10, offset=0):
-    return requests.get(f"https://api.scratch.mit.edu/news?limit={limit}&offset={offset}").json()
+    return requests.get(f"https://apiscratch.synt2x.xyz/news?limit={limit}&offset={offset}").json()
 
 def featured_projects():
-    return requests.get("https://api.scratch.mit.edu/proxy/featured").json()["community_featured_projects"]
+    return requests.get("https://apiscratch.synt2x.xyz/proxy/featured").json()["community_featured_projects"]
 
 def featured_studios():
-    return requests.get("https://api.scratch.mit.edu/proxy/featured").json()["community_featured_studios"]
+    return requests.get("https://apiscratch.synt2x.xyz/proxy/featured").json()["community_featured_studios"]
 
 def top_loved():
-    return requests.get("https://api.scratch.mit.edu/proxy/featured").json()["community_most_loved_projects"]
+    return requests.get("https://apiscratch.synt2x.xyz/proxy/featured").json()["community_most_loved_projects"]
 
 def top_remixed():
-    return requests.get("https://api.scratch.mit.edu/proxy/featured").json()["community_most_remixed_projects"]
+    return requests.get("https://apiscratch.synt2x.xyz/proxy/featured").json()["community_most_remixed_projects"]
 
 def newest_projects():
-    return requests.get("https://api.scratch.mit.edu/proxy/featured").json()["community_newest_projects"]
+    return requests.get("https://apiscratch.synt2x.xyz/proxy/featured").json()["community_newest_projects"]
 
 def curated_projects():
-    return requests.get("https://api.scratch.mit.edu/proxy/featured").json()["curator_top_projects"]
+    return requests.get("https://apiscratch.synt2x.xyz/proxy/featured").json()["curator_top_projects"]
 
 def design_studio_projects():
     return requests.get("https://api.scratch.mit.edu/proxy/featured").json()["scratch_design_studio"]
@@ -44,27 +44,27 @@ def search_posts(*, query, order="newest", page=0):
         return []
 
 def total_site_stats():
-    data = requests.get("https://scratch.mit.edu/statistics/data/daily/").json()
+    data = requests.get("https://scratch.synt2x.xyz/statistics/data/daily/").json()
     data.pop("_TS")
     return data
 
 def monthly_site_traffic():
-    data = requests.get("https://scratch.mit.edu/statistics/data/monthly-ga/").json()
+    data = requests.get("https://scratch.synt2x.xyz/statistics/data/monthly-ga/").json()
     data.pop("_TS")
     return data
 
 def country_counts():
-    return requests.get("https://scratch.mit.edu/statistics/data/monthly/").json()["country_distribution"]
+    return requests.get("https://scratch.synt2x.xyz/statistics/data/monthly/").json()["country_distribution"]
 
 def age_distribution():
-    data = requests.get("https://scratch.mit.edu/statistics/data/monthly/").json()["age_distribution_data"][0]["values"]
+    data = requests.get("https://scratch.synt2x.xyz/statistics/data/monthly/").json()["age_distribution_data"][0]["values"]
     return_data = {}
     for value in data:
         return_data[value["x"]] = value["y"]
     return return_data
 
 def get_health():
-    return requests.get("https://api.scratch.mit.edu/health").json()
+    return requests.get("https://apiscratch.synt2x.xyz/health").json()
 
 def get_csrf_token():
     """
@@ -74,5 +74,5 @@ def get_csrf_token():
         str: The generated scratchcsrftoken
     """
     return requests.get(
-        "https://scratch.mit.edu/csrf_token/"
+        "https://scratch.synt2x.xyz/csrf_token/"
     ).headers["set-cookie"].split(";")[3][len(" Path=/, scratchcsrftoken="):]
